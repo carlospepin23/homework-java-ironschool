f450b949cf239d6c100149dfc95c795737bb954f
carlospepin23 <carlos.pepin1@upr.edu>
2024-06-22 13:47:47 -0400

    created app & command classes & implemented logic

---
+++ Ironschool-project/src/main/java/com/ironhack/Command.java
@@ -0,0 +1,14 @@
+package com.ironhack;
+
+public enum Command {
+    ENROLL,
+    ASSIGN,
+    SHOW_COURSES,
+    LOOKUP_COURSE,
+    SHOW_STUDENTS,
+    LOOKUP_STUDENT,
+    SHOW_TEACHERS,
+    LOOKUP_TEACHER,
+    SHOW_PROFIT,
+    EXIT
+}
